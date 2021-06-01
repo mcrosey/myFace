6c78ebb63a01fca4893d022f8786a3b1acf1a93a
Michelle Chute <michellechute@icloud.com>
2021-06-01 19:32:45 -0400

    sending

--- server/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    "mongoURI": "mongodb+srv://mcrosey:1qaz!QAZ@cluster0.weeoi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    "JWT_SECRET": "vnijasg94etqm1239daasjno"
-}
