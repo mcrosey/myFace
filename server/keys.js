46c2d72751fa9d839b1ce32eed2aa176cb88eb67
mcrosey <80073115+mcrosey@users.noreply.github.com>
2021-08-04 11:19:27 -0400

    Add files via upload

---
+++ server/keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    "mongoURI": "mongodb+srv://mcrosey:1qaz!QAZ@cluster0.weeoi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "JWT_SECRET": "vnijasg94etqm1239daasjno"
+}
